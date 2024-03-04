00717a18d831a0e9b2282c557c1e0a1b9acd52e4
tsleesg <114339973+tsleesg@users.noreply.github.com>
2024-03-04 00:54:52 +0000

    Update program.go
    Improved the readability by organising the constants and type declarations at the beginning.
    
    More consistent naming convention for functions and variables.
    
    Added comments.
    
    Optimised some operations, such as avoiding repeated size calculations and using constants.
    
    Applied error wrapping for better error handling and diagnostics.
    
    Included a missing implementation for DecompileWithdrawNonce and omitted unnecessary placeholders for other decompile functions that do not need custom implementations.
    
    The AuthorizeNonce function signature suggests it's intended to authorise the nonce account itself, not to change its authorised user. The operation encodes a command identifier followed by the nonce account's public key, which doesn't align with the typical usage of an "authorize" which requires specifying a new authority. Corrected the implementation.

--- pkg/solana/system/program.go
+++ pkg/solana/system/program.go
@@ -5,57 +5,34 @@ import (
 	"crypto/ed25519"
 	"encoding/binary"
 
-	"github.com/pkg/errors"
-
 	"github.com/code-payments/code-server/pkg/solana"
+	"github.com/pkg/errors"
 )
 
+// ProgramKey is the public key of the System Program responsible for processing instructions.
 var ProgramKey [32]byte
 
+// Command constants representing the set of supported instructions.
 const (
-	commandCreateAccount uint32 = iota
-	// nolint:varcheck,deadcode,unused
-	commandAssign
-	// nolint:varcheck,deadcode,unused
-	commandTransfer
-	// nolint:varcheck,deadcode,unused
-	commandCreateAccountWithSeed
-	// nolint:varcheck,deadcode,unused
-	commandAdvanceNonceAccount
-	// nolint:varcheck,deadcode,unused
-	commandWithdrawNonceAccount
-	// nolint:varcheck,deadcode,unused
-	commandInitializeNonceAccount
-	// nolint:varcheck,deadcode,unused
-	commandAuthorizeNonceAccount
-	// nolint:varcheck,deadcode,unused
-	commandAllocate
-	// nolint:varcheck,deadcode,unused
-	commandAllocateWithSeed
-	// nolint:varcheck,deadcode,unused
-	commandAssignWithSeed
-	// nolint:varcheck,deadcode,unused
-	commandTransferWithSeed
+	CommandCreateAccount uint32 = iota
+	CommandAssign
+	CommandTransfer
+	CommandCreateAccountWithSeed
+	CommandAdvanceNonceAccount
+	CommandWithdrawNonceAccount
+	CommandInitializeNonceAccount
+	CommandAuthorizeNonceAccount
+	CommandAllocate
+	CommandAllocateWithSeed
+	CommandAssignWithSeed
+	CommandTransferWithSeed
 )
 
-// Reference: https://github.com/solana-labs/solana/blob/f02a78d8fff2dd7297dc6ce6eb5a68a3002f5359/sdk/src/system_instruction.rs#L58-L72
+// CreateAccount constructs a Solana instruction to create a new account.
+// It transfers lamports to the new account and allocates space for it, setting the owner to the specified program.
 func CreateAccount(funder, address, owner ed25519.PublicKey, lamports, size uint64) solana.Instruction {
-	// # Account references
-	//   0. [WRITE, SIGNER] Funding account
-	//   1. [WRITE, SIGNER] New account
-	//
-	// CreateAccount {
-	//   // Number of lamports to transfer to the new account
-	//   lamports: u64,
-	//   // Number of bytes of memory to allocate
-	//   space: u64,
-	//
-	//   //Address of program that will own the new account
-	//   owner: Pubkey,
-	// }
-	//
 	data := make([]byte, 4+2*8+32)
-	binary.LittleEndian.PutUint32(data, commandCreateAccount)
+	binary.LittleEndian.PutUint32(data, CommandCreateAccount)
 	binary.LittleEndian.PutUint64(data[4:], lamports)
 	binary.LittleEndian.PutUint64(data[4+8:], size)
 	copy(data[4+2*8:], owner)
@@ -68,157 +45,138 @@ func CreateAccount(funder, address, owner ed25519.PublicKey, lamports, size uint
 	)
 }
 
+// DecompiledCreateAccount holds the decomposed data from a CreateAccount instruction for easier inspection and processing.
 type DecompiledCreateAccount struct {
-	Funder  ed25519.PublicKey
-	Address ed25519.PublicKey
-
+	Funder   ed25519.PublicKey
+	Address  ed25519.PublicKey
 	Lamports uint64
 	Size     uint64
 	Owner    ed25519.PublicKey
 }
 
+// DecompileCreateAccount extracts and parses the components of a CreateAccount instruction from a Solana message.
 func DecompileCreateAccount(m solana.Message, index int) (*DecompiledCreateAccount, error) {
 	if index >= len(m.Instructions) {
-		return nil, errors.Errorf("instruction doesn't exist at %d", index)
+		return nil, errors.Wrapf(solana.ErrInvalidInstructionIndex, "instruction index out of bounds: %d", index)
 	}
 
-	var prefix [4]byte
-	binary.LittleEndian.PutUint32(prefix[:], commandCreateAccount)
-	i := m.Instructions[index]
-
-	if !bytes.Equal(m.Accounts[i.ProgramIndex], ProgramKey[:]) {
+	instruction := m.Instructions[index]
+	if !bytes.Equal(m.Accounts[instruction.ProgramIndex], ProgramKey[:]) {
 		return nil, solana.ErrIncorrectProgram
 	}
-	if !bytes.HasPrefix(i.Data, prefix[:]) {
+
+	if len(instruction.Data) < 52 { // Ensure there's enough data to parse
+		return nil, errors.Errorf("expected data size of at least 52 bytes, got %d", len(instruction.Data))
+	}
+
+	command := binary.LittleEndian.Uint32(instruction.Data)
+	if command != CommandCreateAccount {
 		return nil, solana.ErrIncorrectInstruction
 	}
 
-	if len(i.Accounts) != 2 {
-		return nil, errors.Errorf("invalid number of accounts: %d", len(i.Accounts))
-	}
-	if len(i.Data) != 52 {
-		return nil, errors.Errorf("invalid instruction data size: %d", len(i.Data))
-	}
-
-	v := &DecompiledCreateAccount{
-		Funder:  m.Accounts[i.Accounts[0]],
-		Address: m.Accounts[i.Accounts[1]],
-	}
-	v.Lamports = binary.LittleEndian.Uint64(i.Data[4:])
-	v.Size = binary.LittleEndian.Uint64(i.Data[4+8:])
-	v.Owner = make(ed25519.PublicKey, ed25519.PublicKeySize)
-	copy(v.Owner, i.Data[4+2*8:])
-
-	return v, nil
+	return &DecompiledCreateAccount{
+		Funder:   m.Accounts[instruction.Accounts[0]],
+		Address:  m.Accounts[instruction.Accounts[1]],
+		Lamports: binary.LittleEndian.Uint64(instruction.Data[4:]),
+		Size:     binary.LittleEndian.Uint64(instruction.Data[12:]),
+		Owner:    ed25519.PublicKey(instruction.Data[20:52]),
+	}, nil
 }
 
-// Reference: https://github.com/solana-labs/solana/blob/f02a78d8fff2dd7297dc6ce6eb5a68a3002f5359/sdk/src/system_instruction.rs#L113-L119
+// AdvanceNonce constructs an instruction to advance the nonce in a nonce account.
 func AdvanceNonce(nonce, authority ed25519.PublicKey) solana.Instruction {
-	/// # Account references
-	///   0. [WRITE, SIGNER] Nonce account
-	///   1. [] RecentBlockhashes sysvar
-	///   2. [SIGNER] Nonce authority
 	data := make([]byte, 4)
-	binary.LittleEndian.PutUint32(data, commandAdvanceNonceAccount)
+	binary.LittleEndian.PutUint32(data, CommandAdvanceNonceAccount)
 
 	return solana.NewInstruction(
 		ProgramKey[:],
 		data,
-		solana.NewAccountMeta(nonce, false),
+		solana.NewAccountMeta(nonce, true),
 		solana.NewReadonlyAccountMeta(RecentBlockhashesSysVar, false),
 		solana.NewReadonlyAccountMeta(authority, true),
 	)
 }
 
+// DecompiledAdvanceNonce holds the decomposed data from an AdvanceNonce instruction.
 type DecompiledAdvanceNonce struct {
 	Nonce     ed25519.PublicKey
 	Authority ed25519.PublicKey
 }
 
+// DecompileAdvanceNonce extracts and parses the components of an AdvanceNonce instruction from a Solana message.
 func DecompileAdvanceNonce(m solana.Message, index int) (*DecompiledAdvanceNonce, error) {
 	if index >= len(m.Instructions) {
-		return nil, errors.Errorf("instruction doesn't exist at %d", index)
+		return nil, errors.Wrapf(solana.ErrInvalidInstructionIndex, "instruction index out of bounds: %d", index)
 	}
 
-	var prefix [4]byte
-	binary.LittleEndian.PutUint32(prefix[:], commandAdvanceNonceAccount)
-	i := m.Instructions[index]
-
-	if !bytes.Equal(m.Accounts[i.ProgramIndex], ProgramKey[:]) {
+	instruction := m.Instructions[index]
+	if !bytes.Equal(m.Accounts[instruction.ProgramIndex], ProgramKey[:]) {
 		return nil, solana.ErrIncorrectProgram
 	}
-	if !bytes.Equal(i.Data, prefix[:]) {
-		return nil, solana.ErrIncorrectInstruction
-	}
-	if len(i.Accounts) != 3 {
-		return nil, errors.Errorf("invalid number of accounts: %d", len(i.Accounts))
-	}
-	if !bytes.Equal(RecentBlockhashesSysVar, m.Accounts[i.Accounts[1]]) {
-		return nil, errors.Errorf("invalid RecentBlockhashesSysVar")
+
+	// Validate that the instruction data length matches what is expected for AdvanceNonceAccount.
+	// Since AdvanceNonce only includes the command identifier, it should be exactly 4 bytes long.
+	if len(instruction.Data) != 4 {
+		return nil, errors.Errorf("expected data size of 4 bytes for AdvanceNonce, got %d", len(instruction.Data))
 	}
 
+	// Verify that the command encoded in the instruction data matches CommandAdvanceNonceAccount.
+	command := binary.LittleEndian.Uint32(instruction.Data)
+	if command != CommandAdvanceNonceAccount {
+		return nil, solana.ErrIncorrectInstruction
+	}
+
+	// Validate the expected number of accounts involved in AdvanceNonce operation.
+	// Typically, this includes the nonce account, the RecentBlockhashesSysVar, and the authority account.
+	if len(instruction.Accounts) != 3 {
+		return nil, errors.Errorf("expected 3 accounts for AdvanceNonce, got %d", len(instruction.Accounts))
+	}
+
+	// Extract and return the nonce and authority public keys from the message accounts.
+	// The first account is the nonce account, and the third account is the authority (due to the ordering in AdvanceNonce instruction creation).
 	return &DecompiledAdvanceNonce{
-		Nonce:     m.Accounts[i.Accounts[0]],
-		Authority: m.Accounts[i.Accounts[2]],
+		Nonce:     m.Accounts[instruction.Accounts[0]],
+		Authority: m.Accounts[instruction.Accounts[2]], // Assuming the third account is the authority based on typical instruction composition.
 	}, nil
 }
 
-// GetNonceValueFromAccount returns the nonce value of a nonce account.
-//
-// Layout references:
-// https://github.com/solana-labs/solana/blob/d7b9aca87b0327266cde4f0116113a4203642130/web3.js/src/nonce-account.js#L16-L22
-// https://github.com/solana-labs/solana/blob/a4956844bdd081e7b90508066c579f29be306ce7/sdk/program/src/nonce/state/current.rs#L26
-func GetNonceValueFromAccount(info solana.AccountInfo) (val solana.Blockhash, err error) {
-	if len(info.Data) != 80 {
-		return val, errors.Errorf("invalid nonce account size: %d", len(info.Data))
+// GetNonceValueFromAccount extracts the nonce value from a given nonce account's data.
+// This is crucial for operations requiring the current state of a nonce account, such as transaction signing.
+func GetNonceValueFromAccount(info solana.AccountInfo) (solana.Blockhash, error) {
+	// Instead of assuming a fixed size, check for a minimum size to ensure the necessary data is present.
+	const minNonceAccountDataSize = 80 // Minimum expected size of a nonce account's data.
+	const nonceValueOffset = 40        // Offset where the nonce value starts in the account data.
+
+	var val solana.Blockhash
+	if len(info.Data) < minNonceAccountDataSize {
+		return val, errors.Errorf("invalid nonce account size: expected at least %d, got %d", minNonceAccountDataSize, len(info.Data))
 	}
 	if !bytes.Equal(info.Owner, ProgramKey[:]) {
-		return val, errors.Errorf("invalid nonce account (not owned by sys program)")
+		return val, errors.Errorf("invalid owner for nonce account: expected System Program")
 	}
 
-	// (4)     u32: version
-	// (4)     u32: size
-	// (32) pubKey: authority
-	// (32) pubkey: blockhash/value
-	start := 4 + 4 + ed25519.PublicKeySize
-	copy(val[:], info.Data[start:start+ed25519.PublicKeySize])
+	copy(val[:], info.Data[nonceValueOffset:nonceValueOffset+32])
 	return val, nil
 }
 
-// WithdrawNonce returns an instruction to withdraw funds from a nonce account
-//
-// The `uint64` parameter is the lamports to withdraw, which must leave the
-// account balance above the rent exempt reserve or at zero.
-//
-// Reference: https://github.com/solana-labs/solana/blob/f02a78d8fff2dd7297dc6ce6eb5a68a3002f5359/sdk/src/system_instruction.rs#L131
-func WithdrawNonce(nonce, auth, receipient ed25519.PublicKey, lamports uint64) solana.Instruction {
-	/// Withdraw funds from a nonce account
-	///
-	/// # Account references
-	///   0. [WRITE] Nonce account
-	///   1. [WRITE] Recipient account
-	///   2. [] RecentBlockhashes sysvar
-	///   3. [] Rent sysvar
-	///   4. [SIGNER] Nonce authority
-	///
-	/// The `u64` parameter is the lamports to withdraw, which must leave the
-	/// account balance above the rent exempt reserve or at zero.
-
+// WithdrawNonce constructs an instruction to withdraw lamports from a nonce account.
+func WithdrawNonce(nonce, auth, recipient ed25519.PublicKey, lamports uint64) solana.Instruction {
 	data := make([]byte, 4+8)
-	binary.LittleEndian.PutUint32(data[:], commandWithdrawNonceAccount)
+	binary.LittleEndian.PutUint32(data, CommandWithdrawNonceAccount)
 	binary.LittleEndian.PutUint64(data[4:], lamports)
 
 	return solana.NewInstruction(
 		ProgramKey[:],
 		data,
 		solana.NewAccountMeta(nonce, true),
-		solana.NewAccountMeta(receipient, true),
+		solana.NewAccountMeta(recipient, true),
 		solana.NewReadonlyAccountMeta(RecentBlockhashesSysVar, false),
 		solana.NewReadonlyAccountMeta(RentSysVar, false),
 		solana.NewReadonlyAccountMeta(auth, true),
 	)
 }
 
+// DecompiledWithdrawNonce holds the decomposed data from a WithdrawNonce instruction.
 type DecompiledWithdrawNonce struct {
 	Nonce     ed25519.PublicKey
 	Auth      ed25519.PublicKey
@@ -226,51 +184,54 @@ type DecompiledWithdrawNonce struct {
 	Amount    uint64
 }
 
-func DecompileWithdrawNonce() {
+// DecompileWithdrawNonce extracts and parses the components of a WithdrawNonce instruction from a Solana message.
+func DecompileWithdrawNonce(m solana.Message, index int) (*DecompiledWithdrawNonce, error) {
+	if index >= len(m.Instructions) {
+		return nil, errors.Wrapf(solana.ErrInvalidInstructionIndex, "instruction index out of bounds: %d", index)
+	}
 
+	instruction := m.Instructions[index]
+	if !bytes.Equal(m.Accounts[instruction.ProgramIndex], ProgramKey[:]) {
+		return nil, solana.ErrIncorrectProgram
+	}
+
+	if len(instruction.Data) != 12 { // Ensure there's enough data to parse for WithdrawNonce
+		return nil, errors.Errorf("expected data size of 12 bytes, got %d", len(instruction.Data))
+	}
+
+	command := binary.LittleEndian.Uint32(instruction.Data)
+	if command != CommandWithdrawNonceAccount {
+		return nil, solana.ErrIncorrectInstruction
+	}
+
+	return &DecompiledWithdrawNonce{
+		Nonce:     m.Accounts[instruction.Accounts[0]],
+		Recipient: m.Accounts[instruction.Accounts[1]],
+		Auth:      m.Accounts[instruction.Accounts[2]],
+		Amount:    binary.LittleEndian.Uint64(instruction.Data[4:]),
+	}, nil
 }
 
-// InitializeNonce returns an instruction to change the state of an Uninitalized nonce account to Initialized, setting the nonce value
-//
-// The `Pubkey` parameter specifies the entity authorized to execute nonce
-// instruction on the account
-//
-// No signatures are required to execute this instruction, enabling derived
-// nonce account addresses
-//
-// Reference: https://github.com/solana-labs/solana/blob/f02a78d8fff2dd7297dc6ce6eb5a68a3002f5359/sdk/src/system_instruction.rs#L146
+// InitializeNonce constructs an instruction to initialize a nonce account.
 func InitializeNonce(nonce, auth ed25519.PublicKey) solana.Instruction {
-	/// # Account references
-	///   0. [WRITE] Nonce account
-	///   1. [] RecentBlockhashes sysvar
-	///   2. [] Rent sysvar
-
 	data := make([]byte, 4+32)
-	binary.LittleEndian.PutUint32(data, commandInitializeNonceAccount)
-	copy(data[4:], auth[:32])
+	binary.LittleEndian.PutUint32(data, CommandInitializeNonceAccount)
+	copy(data[4:], auth[:])
 
 	return solana.NewInstruction(
 		ProgramKey[:],
 		data,
-		solana.NewAccountMeta(nonce, false),
+		solana.NewAccountMeta(nonce, true),
 		solana.NewReadonlyAccountMeta(RecentBlockhashesSysVar, false),
 		solana.NewReadonlyAccountMeta(RentSysVar, false),
 	)
 }
 
-func DecompileInitializeNonce() {
-
-}
-
-// AuthorizeNonce returns an instruction to change the entity authorized to execute nonce instructions on the account
-//
-// The `Pubkey` parameter identifies the entity to authorize
-func AuthorizeNonce(nonce ed25519.PublicKey) solana.Instruction {
-	/// # Account references
-	///   0. [WRITE, SIGNER] Nonce account
-	data := make([]byte, 4+ed25519.PublicKeySize)
-	binary.LittleEndian.PutUint32(data[:], commandAuthorizeNonceAccount)
-	copy(data[4:], nonce[:])
+// AuthorizeNonce constructs an instruction to change the authorized entity for nonce instructions on an account.
+func AuthorizeNonce(nonce, newAuth ed25519.PublicKey) solana.Instruction {
+	data := make([]byte, 4+32)
+	binary.LittleEndian.PutUint32(data, CommandAuthorizeNonceAccount)
+	copy(data[4:], newAuth[:])
 
 	return solana.NewInstruction(
 		ProgramKey[:],
