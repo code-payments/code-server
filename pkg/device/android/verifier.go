c332b5a11f41d96f46e2d535cc287df5fb7c446a
Jeff Yanta <jeff@getcode.com>
2024-03-05 09:11:26 -0500

    Validate Firebase app check tokens coming from Android devices

--- pkg/device/android/verifier.go
+++ pkg/device/android/verifier.go
@@ -2,7 +2,10 @@ package android
 
 import (
 	"context"
+	"time"
 
+	firebase "firebase.google.com/go/v4"
+	"firebase.google.com/go/v4/appcheck"
 	"github.com/code-payments/code-server/pkg/device"
 	"github.com/code-payments/code-server/pkg/metrics"
 )
@@ -12,22 +15,71 @@ const (
 )
 
 type androidDeviceVerifier struct {
+	projectId string
+	appId     string
+	client    *appcheck.Client
 }
 
 // NewAndroidDeviceVerifier returns a new device.Verifier for Android devices
-func NewAndroidDeviceVerifier() (device.Verifier, error) {
-	return &androidDeviceVerifier{}, nil
+//
+// Requires GOOGLE_APPLICATION_CREDENTIALS to be set:
+// https://firebase.google.com/docs/admin/setup#initialize-sdk
+func NewAndroidDeviceVerifier(
+	projectId, appId string,
+) (device.Verifier, error) {
+	firebaseApp, err := firebase.NewApp(context.Background(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	appCheckClient, err := firebaseApp.AppCheck(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return &androidDeviceVerifier{
+		projectId: projectId,
+		appId:     appId,
+		client:    appCheckClient,
+	}, nil
 }
 
 // IsValid implements device.Verifier.IsValid
-//
-// todo: implement me
 func (v *androidDeviceVerifier) IsValid(ctx context.Context, token string) (bool, error) {
 	tracer := metrics.TraceMethodCall(ctx, metricsStructName, "IsValid")
 	defer tracer.End()
 
 	isValid, err := func() (bool, error) {
-		return false, nil
+		decodedToken, err := v.client.VerifyToken(token)
+		if err != nil {
+			return false, nil
+		}
+
+		// Ensure the token is for the official project
+		var foundProjectId bool
+		for _, audience := range decodedToken.Audience {
+			if audience == "projects/"+v.projectId {
+				foundProjectId = true
+			}
+		}
+		if !foundProjectId {
+			return false, nil
+		}
+
+		// Ensure the token is for the official application
+		if decodedToken.AppID != v.appId {
+			return false, nil
+		}
+
+		// Require very fresh tokens
+		if time.Since(decodedToken.IssuedAt) > time.Minute {
+			return false, nil
+		}
+
+		// todo: Consume the token to better avoid replay attacks when available
+		//       https://firebase.google.com/docs/app-check/custom-resource-backend#replay-protection
+
+		return true, nil
 	}()
 
 	if err != nil {
