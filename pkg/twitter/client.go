f4d1cab857be059879b223e583c55d788dc16d90
jeffyanta <jeff@getcode.com>
2024-03-25 13:26:36 -0400

    Update Twitter client to get recent tweets for a user (#89)

--- pkg/twitter/client.go
+++ pkg/twitter/client.go
@@ -13,9 +13,7 @@ import (
 )
 
 const (
-	baseUrl              = "https://api.twitter.com/2/"
-	getUserByIdUrl       = baseUrl + "users/"
-	getUserByUsernameUrl = baseUrl + "users/by/username/"
+	baseUrl = "https://api.twitter.com/2/"
 
 	metricsStructName = "twitter.client"
 )
@@ -42,12 +40,18 @@ type User struct {
 	PublicMetrics   PublicMetrics `json:"public_metrics"`
 }
 
-// PublicMetrics are public metrics for a Twitter user
+// PublicMetrics represents the structure for public metrics in the Twitter API response
 type PublicMetrics struct {
 	FollowersCount int `json:"followers_count"`
 	FollowingCount int `json:"following_count"`
 	TweetCount     int `json:"tweet_count"`
 	LikeCount      int `json:"like_count"`
+}
+
+// Tweet represents the structure for a tweet in the Twitter API response
+type Tweet struct {
+	ID   string `json:"id"`
+	Text string `json:"text"`
 }
 
 // GetUserById makes a request to the Twitter API and returns the user's information
@@ -56,7 +60,7 @@ func (c *Client) GetUserById(ctx context.Context, id string) (*User, error) {
 	tracer := metrics.TraceMethodCall(ctx, metricsStructName, "GetUserById")
 	defer tracer.End()
 
-	user, err := c.getUser(ctx, getUserByIdUrl+id)
+	user, err := c.getUser(ctx, baseUrl+"users/"+id)
 	if err != nil {
 		tracer.OnError(err)
 	}
@@ -69,11 +73,70 @@ func (c *Client) GetUserByUsername(ctx context.Context, username string) (*User,
 	tracer := metrics.TraceMethodCall(ctx, metricsStructName, "GetUserByUsername")
 	defer tracer.End()
 
-	user, err := c.getUser(ctx, getUserByUsernameUrl+username)
+	user, err := c.getUser(ctx, baseUrl+"users/by/username/"+username)
 	if err != nil {
 		tracer.OnError(err)
 	}
 	return user, err
+}
+
+// GetUserTweets gets tweets for a given user
+//
+// todo: Doesn't support paging, so only the most recent ones are returned
+func (c *Client) GetUserTweets(ctx context.Context, userId string, maxResults int) ([]Tweet, error) {
+	tracer := metrics.TraceMethodCall(ctx, metricsStructName, "GetUserTweets")
+	defer tracer.End()
+
+	tweets, err := func() ([]Tweet, error) {
+		url := fmt.Sprintf(baseUrl+"users/"+userId+"/tweets?max_results=%d", maxResults)
+
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		req.Header.Add("Authorization", "Bearer "+c.apiToken)
+
+		resp, err := c.client.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				return nil, err
+			}
+			fmt.Println(string(body))
+
+			return nil, fmt.Errorf("unexpected http status code: %d", resp.StatusCode)
+		}
+
+		var result struct {
+			Data   []Tweet        `json:"data"`
+			Errors []twitterError `json:"errors"`
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := json.Unmarshal(body, &result); err != nil {
+			return nil, err
+		}
+
+		if len(result.Errors) > 0 {
+			return nil, result.Errors[0].toError()
+		}
+		return result.Data, nil
+	}()
+
+	if err != nil {
+		tracer.OnError(err)
+	}
+	return tweets, err
 }
 
 func (c *Client) getUser(ctx context.Context, fromUrl string) (*User, error) {
