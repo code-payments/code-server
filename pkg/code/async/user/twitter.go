4d7ab612ce4d7207403371f996bd47d4d2680e5d
Jeff Yanta <jeff@getcode.com>
2024-04-08 11:51:08 -0700

    Make adjustments to Twitter worker with new registration flow

--- pkg/code/async/user/twitter.go
+++ pkg/code/async/user/twitter.go
@@ -2,9 +2,12 @@ package async_user
 
 import (
 	"context"
+	"crypto/ed25519"
+	"database/sql"
 	"strings"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/mr-tron/base58"
 	"github.com/newrelic/go-agent/v3/newrelic"
 	"github.com/pkg/errors"
@@ -21,7 +24,7 @@ import (
 )
 
 const (
-	tipCardRegistrationPrefix = "CodeAccount:"
+	tipCardRegistrationPrefix = "CodeAccount"
 	maxTweetSearchResults     = 100 // maximum allowed
 )
 
@@ -44,7 +47,7 @@ func (p *service) twitterRegistrationWorker(serviceCtx context.Context, interval
 			defer m.End()
 			tracedCtx := newrelic.NewContext(serviceCtx, m)
 
-			err = p.findNewTwitterRegistrations(tracedCtx)
+			err = p.processNewTwitterRegistrations(tracedCtx)
 			if err != nil {
 				m.NoticeError(err)
 				log.WithError(err).Warn("failure processing new twitter registrations")
@@ -98,96 +101,52 @@ func (p *service) twitterUserInfoUpdateWorker(serviceCtx context.Context, interv
 	return err
 }
 
-func (p *service) findNewTwitterRegistrations(ctx context.Context) error {
-	var newlyProcessedTweets []string
-
-	err := func() error {
-		var pageToken *string
-		processedUsernames := make(map[string]any)
-		for {
-			tweets, nextPageToken, err := p.twitterClient.SearchRecentTweets(
-				ctx,
-				tipCardRegistrationPrefix,
-				maxTweetSearchResults,
-				pageToken,
-			)
-			if err != nil {
-				return errors.Wrap(err, "error searching tweets")
-			}
-
-			for _, tweet := range tweets {
-				if tweet.AdditionalMetadata.Author == nil {
-					return errors.Errorf("author missing in tweet %s", tweet.ID)
-				}
-
-				isTweetProcessed, err := p.data.IsTweetProcessed(ctx, tweet.ID)
-				if err != nil {
-					return errors.Wrap(err, "error checking if tweet is processed")
-				} else if isTweetProcessed {
-					// Found a checkpoint, so stop processing
-					return nil
-				}
-
-				// Oldest tweets go first, so we are guaranteed to checkpoint everything
-				newlyProcessedTweets = append([]string{tweet.ID}, newlyProcessedTweets...)
-
-				// Avoid reprocessing a Twitter user and potentially overriding the
-				// tip address with something older.
-				if _, ok := processedUsernames[tweet.AdditionalMetadata.Author.Username]; ok {
-					continue
-				}
-
-				// Attempt to find a tip account from the registration tweet
-				tipAccount, err := findTipAccountRegisteredInTweet(tweet)
-				switch err {
-				case nil:
-				case errTwitterInvalidRegistrationValue, errTwitterRegistrationNotFound:
-					continue
-				default:
-					return errors.Wrapf(err, "unexpected error processing tweet %s", tweet.ID)
-				}
-
-				// Validate the new tip account
-				accountInfoRecord, err := p.data.GetAccountInfoByTokenAddress(ctx, tipAccount.PublicKey().ToBase58())
-				switch err {
-				case nil:
-					// todo: potentially use a relationship account instead
-					if accountInfoRecord.AccountType != commonpb.AccountType_PRIMARY {
-						continue
-					}
-				case account.ErrAccountInfoNotFound:
-					continue
-				default:
-					return errors.Wrap(err, "error getting account info")
-				}
-
-				processedUsernames[tweet.AdditionalMetadata.Author.Username] = struct{}{}
-
-				err = p.updateCachedTwitterUser(ctx, tweet.AdditionalMetadata.Author, tipAccount)
-				if err != nil {
-					return errors.Wrap(err, "error updating cached user state")
-				}
-			}
-
-			if nextPageToken == nil {
-				return nil
-			}
-			pageToken = nextPageToken
-		}
-	}()
-
+func (p *service) processNewTwitterRegistrations(ctx context.Context) error {
+	tweets, err := p.findNewRegistrationTweets(ctx)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "error finding new registration tweets")
 	}
 
-	// Only update the processed tweet cache once we've found another checkpoint,
-	// or reached the end of the Tweet feed.
-	//
-	// todo: add batching
-	for _, tweetId := range newlyProcessedTweets {
-		err := p.data.MarkTweetAsProcessed(ctx, tweetId)
-		if err != nil {
-			return errors.Wrap(err, "error marking tweet as processed")
+	for _, tweet := range tweets {
+		if tweet.AdditionalMetadata.Author == nil {
+			return errors.Errorf("author missing in tweet %s", tweet.ID)
+		}
+
+		// Attempt to find a verified tip account from the registration tweet
+		tipAccount, registrationNonce, err := p.findVerifiedTipAccountRegisteredInTweet(ctx, tweet)
+		switch err {
+		case nil:
+		case errTwitterInvalidRegistrationValue, errTwitterRegistrationNotFound:
+			continue
+		default:
+			return errors.Wrapf(err, "unexpected error processing tweet %s", tweet.ID)
+		}
+
+		// Save the updated tipping information. Any race conditions with duplicate
+		// nonces or tip addresses will be caught and ignored later.
+		err = p.data.ExecuteInTx(ctx, sql.LevelDefault, func(ctx context.Context) error {
+			err = p.data.MarkTwitterNonceAsUsed(ctx, tweet.ID, *registrationNonce)
+			if err != nil {
+				return err
+			}
+
+			err = p.updateCachedTwitterUser(ctx, tweet.AdditionalMetadata.Author, tipAccount)
+			if err != nil {
+				return err
+			}
+
+			err = p.data.MarkTweetAsProcessed(ctx, tweet.ID)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		})
+
+		switch err {
+		case nil, twitter.ErrDuplicateTipAddress, twitter.ErrDuplicateNonce:
+		default:
+			return errors.Wrap(err, "error saving new registration")
 		}
 	}
 
@@ -238,38 +197,118 @@ func (p *service) updateCachedTwitterUser(ctx context.Context, user *twitter_lib
 	}
 
 	err = p.data.SaveTwitterUser(ctx, record)
-	if err != nil {
+	switch err {
+	case nil, twitter.ErrDuplicateTipAddress:
+		return err
+	default:
 		return errors.Wrap(err, "error updating cached twitter user")
 	}
-	return nil
 }
 
-func findTipAccountRegisteredInTweet(tweet *twitter_lib.Tweet) (*common.Account, error) {
-	var depositAccount *common.Account
+func (p *service) findNewRegistrationTweets(ctx context.Context) ([]*twitter_lib.Tweet, error) {
+	var pageToken *string
+	var res []*twitter_lib.Tweet
+	for {
+		tweets, nextPageToken, err := p.twitterClient.SearchRecentTweets(
+			ctx,
+			tipCardRegistrationPrefix,
+			maxTweetSearchResults,
+			pageToken,
+		)
+		if err != nil {
+			return nil, errors.Wrap(err, "error searching tweets")
+		}
 
-	parts := strings.Fields(tweet.Text)
-	for _, part := range parts {
-		if !strings.HasPrefix(part, tipCardRegistrationPrefix) {
+		for _, tweet := range tweets {
+			isTweetProcessed, err := p.data.IsTweetProcessed(ctx, tweet.ID)
+			if err != nil {
+				return nil, errors.Wrap(err, "error checking if tweet is processed")
+			} else if isTweetProcessed {
+				// Found a checkpoint
+				return res, nil
+			}
+
+			res = append([]*twitter_lib.Tweet{tweet}, res...)
+		}
+
+		if nextPageToken == nil {
+			return res, nil
+		}
+		pageToken = nextPageToken
+	}
+}
+
+func (p *service) findVerifiedTipAccountRegisteredInTweet(ctx context.Context, tweet *twitter_lib.Tweet) (*common.Account, *uuid.UUID, error) {
+	tweetParts := strings.Fields(tweet.Text)
+	for _, tweetPart := range tweetParts {
+		// Look for the well-known prefix to indicate a potential registration value
+
+		if !strings.HasPrefix(tweetPart, tipCardRegistrationPrefix) {
 			continue
 		}
 
-		part = part[len(tipCardRegistrationPrefix):]
-		part = strings.TrimSuffix(part, ".")
+		// Parse out the individual components of the registration value
 
-		decoded, err := base58.Decode(part)
+		tweetPart = strings.TrimSuffix(tweetPart, ".")
+		registrationParts := strings.Split(tweetPart, ":")
+		if len(registrationParts) != 4 {
+			return nil, nil, errTwitterInvalidRegistrationValue
+		}
+
+		addressString := registrationParts[1]
+		nonceString := registrationParts[2] // todo: ensure it's unique (maybe not here)
+		signatureString := registrationParts[3]
+
+		decodedAddress, err := base58.Decode(addressString)
 		if err != nil {
-			return nil, errTwitterInvalidRegistrationValue
+			return nil, nil, errTwitterInvalidRegistrationValue
+		}
+		if len(decodedAddress) != 32 {
+			return nil, nil, errTwitterInvalidRegistrationValue
+		}
+		tipAccount, _ := common.NewAccountFromPublicKeyBytes(decodedAddress)
+
+		nonce, err := uuid.Parse(nonceString)
+		if err != nil {
+			return nil, nil, errTwitterInvalidRegistrationValue
 		}
 
-		if len(decoded) != 32 {
-			return nil, errTwitterInvalidRegistrationValue
+		decodedSignature, err := base58.Decode(signatureString)
+		if err != nil {
+			return nil, nil, errTwitterInvalidRegistrationValue
+		}
+		if len(decodedSignature) != 64 {
+			return nil, nil, errTwitterInvalidRegistrationValue
 		}
 
-		depositAccount, _ = common.NewAccountFromPublicKeyBytes(decoded)
-		return depositAccount, nil
+		// Validate the components of the registration value
+
+		var tipAuthority *common.Account
+		accountInfoRecord, err := p.data.GetAccountInfoByTokenAddress(ctx, tipAccount.PublicKey().ToBase58())
+		switch err {
+		case nil:
+			if accountInfoRecord.AccountType != commonpb.AccountType_PRIMARY {
+				return nil, nil, errTwitterInvalidRegistrationValue
+			}
+
+			tipAuthority, err = common.NewAccountFromPublicKeyString(accountInfoRecord.AuthorityAccount)
+			if err != nil {
+				return nil, nil, errors.Wrap(err, "invalid tip authority account")
+			}
+		case account.ErrAccountInfoNotFound:
+			return nil, nil, errTwitterInvalidRegistrationValue
+		default:
+			return nil, nil, errors.Wrap(err, "error getting account info")
+		}
+
+		if !ed25519.Verify(tipAuthority.PublicKey().ToBytes(), nonce[:], decodedSignature) {
+			return nil, nil, errTwitterInvalidRegistrationValue
+		}
+
+		return tipAccount, &nonce, nil
 	}
 
-	return nil, errTwitterRegistrationNotFound
+	return nil, nil, errTwitterRegistrationNotFound
 }
 
 func toProtoVerifiedType(value string) userpb.TwitterUser_VerifiedType {
