3035a8f7c724510f2e35a8d7031a89375d515bb7
Jeff Yanta <jeff@getcode.com>
2024-06-07 13:36:25 -0400

    Implement RPCs that operate on chat member state

--- pkg/code/data/chat/v2/store.go
+++ pkg/code/data/chat/v2/store.go
@@ -1,9 +1,33 @@
 package chat_v2
 
+import (
+	"context"
+	"errors"
+)
+
 var (
-// todo: Define well-known errors here
+	ErrChatNotFound    = errors.New("chat not found")
+	ErrMemberNotFound  = errors.New("chat member not found")
+	ErrMessageNotFound = errors.New("chat message not found")
 )
 
 // todo: Define interface methods
 type Store interface {
+	// GetChatById gets a chat by its chat ID
+	GetChatById(ctx context.Context, chatId ChatId) (*ChatRecord, error)
+
+	// GetMemberById gets a chat member by the chat and member IDs
+	GetMemberById(ctx context.Context, chatId ChatId, memberId MemberId) (*MemberRecord, error)
+
+	// GetMessageById gets a chat message by the chat and message IDs
+	GetMessageById(ctx context.Context, chatId ChatId, messageId MessageId) (*MessageRecord, error)
+
+	// AdvancePointer advances a chat pointer for a chat member
+	AdvancePointer(ctx context.Context, chatId ChatId, memberId MemberId, pointerType PointerType, pointer MessageId) error
+
+	// SetMuteState updates the mute state for a chat member
+	SetMuteState(ctx context.Context, chatId ChatId, memberId MemberId, isMuted bool) error
+
+	// SetSubscriptionState updates the subscription state for a chat member
+	SetSubscriptionState(ctx context.Context, chatId ChatId, memberId MemberId, isSubscribed bool) error
 }
