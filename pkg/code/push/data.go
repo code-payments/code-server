77ab896560532d23116a5522355fc92a2cbd5f35
Mike Cheng <mike@ch3ng.dev>
2024-07-26 17:00:17 -0400

    code/push: filter out based on recent appInstallId
    Using latest token doesn't work for a lot of reasons. Instead, we need
    to use the most recent _per device_.

--- pkg/code/push/data.go
+++ pkg/code/push/data.go
@@ -3,12 +3,14 @@ package push
 import (
 	"context"
 	"slices"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 
 	"github.com/code-payments/code-server/pkg/code/common"
 	code_data "github.com/code-payments/code-server/pkg/code/data"
 	push_data "github.com/code-payments/code-server/pkg/code/data/push"
+	"github.com/code-payments/code-server/pkg/pointer"
 	push_lib "github.com/code-payments/code-server/pkg/push"
 )
 
@@ -105,38 +107,72 @@ func sendMutableNotificationToOwner(
 	}
 
 	log.WithField("tokens", pushTokenRecords).Info("Found push tokens")
+
+	// We reverse sort the tokens based on creation date to prioritze
+	// recenty tokens. Furthermore, this means that in the case of a
+	// detected dedupe, we use the most recent token.
 	slices.SortFunc(pushTokenRecords, func(a, b *push_data.Record) int {
-		return a.CreatedAt.Compare(b.CreatedAt)
+		return b.CreatedAt.Compare(a.CreatedAt)
 	})
 
-	pushTokenRecord := pushTokenRecords[len(pushTokenRecords)-1]
-	log = log.WithField("push_token", pushTokenRecord.PushToken)
+	pushedTokens := make(map[string]struct{})
+	pushedDevices := make(map[string]struct{})
 
-	// Try push
-	switch pushTokenRecord.TokenType {
-	case push_data.TokenTypeFcmApns:
-		log.Info("Sending mutable push")
-		err = pusher.SendMutableAPNSPush(
-			ctx,
-			pushTokenRecord.PushToken,
-			titleKey,
-			string(notificationType),
-			titleKey, // All mutable pushes have a thread ID that's the title
-			kvs,
-		)
-	case push_data.TokenTypeFcmAndroid:
-		// todo: anything special required for Android?
-		log.Info("Sending data push")
-		err = pusher.SendDataPush(
-			ctx,
-			pushTokenRecord.PushToken,
-			kvs,
-		)
-	}
+	for _, pushTokenRecord := range pushTokenRecords {
+		if _, ok := pushedTokens[pushTokenRecord.PushToken]; ok {
+			continue
+		}
 
-	if err != nil {
-		log.WithError(err).Warn("failure sending push notification")
-		onPushError(ctx, data, pusher, pushTokenRecord)
+		// In the case of apple, we attempt to dedupe based on app install id
+		var appInstallId string
+		if pushTokenRecord.TokenType == push_data.TokenTypeFcmApns {
+			appInstallId = *pointer.StringOrDefault(pushTokenRecord.AppInstallId, "")
+			if appInstallId == "" {
+				// Legacy token: Attempt to take it from the token itself
+				parts := strings.Split(pushTokenRecord.PushToken, ":")
+				if len(parts) == 2 {
+					appInstallId = parts[0]
+				}
+			}
+
+			if appInstallId != "" {
+				if _, ok := pushedDevices[appInstallId]; ok {
+					continue
+				}
+			}
+		}
+
+		// Try push
+		switch pushTokenRecord.TokenType {
+		case push_data.TokenTypeFcmApns:
+			log.Info("Sending mutable push")
+			err = pusher.SendMutableAPNSPush(
+				ctx,
+				pushTokenRecord.PushToken,
+				titleKey,
+				string(notificationType),
+				titleKey, // All mutable pushes have a thread ID that's the title
+				kvs,
+			)
+		case push_data.TokenTypeFcmAndroid:
+			// todo: anything special required for Android?
+			log.Info("Sending data push")
+			err = pusher.SendDataPush(
+				ctx,
+				pushTokenRecord.PushToken,
+				kvs,
+			)
+		}
+
+		if err != nil {
+			log.WithError(err).Warn("failure sending push notification")
+			onPushError(ctx, data, pusher, pushTokenRecord)
+		} else {
+			pushedTokens[pushTokenRecord.PushToken] = struct{}{}
+			if appInstallId != "" {
+				pushedDevices[appInstallId] = struct{}{}
+			}
+		}
 	}
 
 	return nil
