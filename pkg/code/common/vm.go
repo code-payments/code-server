ba66c01c95addbcd1770430f13fb16b55aacb060
Jeff Yanta <jeff@getcode.com>
2024-11-25 14:18:58 -0500

    Setup official FC VM instance

--- pkg/code/common/vm.go
+++ pkg/code/common/vm.go
@@ -2,8 +2,8 @@ package common
 
 var (
 	// The well-known Code VM instance used by the Flipchat app
-	CodeVmAccount, _ = NewAccountFromPublicKeyString("peuX3mw4B98mRDpno2XpSmkwg8b11GxoKfRskL8kwUm")
+	CodeVmAccount, _ = NewAccountFromPublicKeyString("FDrssd3RVeCkgHAT2NkEpkxC5UgfJpKHeebXUMnuzD6D")
 
 	// The well-known Code VM instance omnibus used by the Flipchat app
-	CodeVmOmnibusAccount, _ = NewAccountFromPublicKeyString("7mhguYUabdjpo1sG6DdiAC5yL3XVJ3WbgycjaMoYZVpq")
+	CodeVmOmnibusAccount, _ = NewAccountFromPublicKeyString("BTbxm64TNDQqhQRxGNEf84Y6g9ofDodrtXRXtGLJt7TK")
 )
